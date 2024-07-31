5f6f3f29ea6427812bd606dee62a0f8f20258ade
@hexlet/cli <support@hexlet.io>
2024-07-31 22:04:42 +0300

    submit java-web-ru/crud

--- java-web-ru/crud/src/main/java/exercise/controller/PostsController.java
+++ java-web-ru/crud/src/main/java/exercise/controller/PostsController.java
@@ -10,13 +10,13 @@ import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
 
 public class PostsController {
-    private static final Integer pageSize = 5;
+    private static final Integer PAGE_SIZE = 5;
 
     // BEGIN
     public static void index(Context ctx) {
         System.out.println(ctx.pathParamMap());
         var pageNum = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1);
-        var posts = PostRepository.findAll(pageNum, pageSize);
+        var posts = PostRepository.findAll(pageNum, PAGE_SIZE);
         var page = new PostsPage(posts, pageNum);
 
         ctx.render("posts/index.jte", model("page", page));
